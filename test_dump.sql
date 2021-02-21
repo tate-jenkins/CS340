5d93931428f336bbb33ecc184c8bbdc8e7a57eaf
vwyborski16osu <72372128+vwyborski16osu@users.noreply.github.com>
2021-02-20 20:32:42 -0800

    Revert "Merge branch 'main' of https://github.com/tate-jenkins/CS340"
    This reverts commit de15c2103be1446cdf030ff7775f79e5be7a24dd, reversing
    changes made to 45d48297552508938e4e5b27afe6df285a6370ba.

--- test_dump.sql
+++ test_dump.sql
@@ -1,10 +1,9 @@
 SET FOREIGN_KEY_CHECKS = 0;
-DROP TABLE IF EXISTS Users_bet_slips;
-DROP TABLE IF EXISTS Parlay;
 DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Users_bet_slips;
 DROP TABLE IF EXISTS Bet_slips;
+DROP TABLE IF EXISTS Parlay;
 DROP TABLE IF EXISTS Games;
-DROP TABLE IF EXISTS Parlay_bet_slips;
 SET FOREIGN_KEY_CHECKS = 1;
 
 --
@@ -24,9 +23,57 @@ UNIQUE KEY (username)
 
 INSERT INTO `Users` (`username`) VALUES
 ('vince'),
-('tate'),
-('pete');
+('tate');
 
+--
+-- Table structure for table `Bet_slips`
+--
+CREATE TABLE Bet_slips (
+slip_id int NOT NULL AUTO_INCREMENT,
+wager numeric(7,2) NOT NULL,
+bet_type varchar(255) NOT NULL,
+bet_won boolean DEFAULT NULL,
+payout_status boolean DEFAULT NULL,
+game_id int(11) NOT NULL,
+parlay_id int(11) DEFAULT NULL,
+PRIMARY KEY (slip_id),
+FOREIGN KEY games2bet_slips (game_id)
+REFERENCES Games
+	ON UPDATE CASCADE
+	ON DELETE CASCADE 
+CHECK(Wager>=0),
+CONSTRAINT CHK_bet_type CHECK(UPPER(bet_type) = 'TEAM_A_MONEY_LINE' OR 
+	UPPER(bet_type) = 'TEAM_B_MONEY_LINE' OR
+	UPPER(bet_type) = 'TEAM_A_SPREAD' OR
+	UPPER(bet_type) = 'TEAM_B_SPREAD' OR
+	UPPER(bet_type) = 'OVER' OR
+	UPPER(bet_type) = 'UNDER')
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Bet_slips`
+--
+
+INSERT INTO `Bet_slips` (`wager`,`bet_type`,`game_id`) VALUES
+('10.00', 'TEAM_A_SPREAD',1),
+('10.00', 'OVER',1);
+
+--
+-- Table structure for table `Parlay`
+--
+CREATE TABLE `Parlay` (
+`primary_key` int(11) NOT NULL AUTO_INCREMENT,
+`parlay_id` int(11) NOT NULL,
+`slip_id` int(11) NOT NULL,
+PRIMARY KEY (`primary_key`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Parlay`
+--
+INSERT INTO `Parlay` (`parlay_id`,`slip_id`) VALUES
+(1,1),
+(1,2);
 --
 -- Table structure for table `Games`
 --
@@ -48,94 +95,22 @@ PRIMARY KEY (`game_id`)
 --
 
 INSERT INTO `Games` (`spread`,`team_a`,`team_b`,`team_a_odds`,`team_b_odds`,`over_under_line`) VALUES
-('03.0','Chiefs','Buccaneers','149','-169','56.5'),
-('07.0','Titans','Rams','142','-175','52.0'),
-('06.5','Bears','Packers','144','-171','58.5');
-
---
--- Table structure for table `Bet_slips`
---
-CREATE TABLE `Bet_slips` (
-`slip_id` int(11) NOT NULL AUTO_INCREMENT,
-`wager` numeric(7,2) NOT NULL,
-`bet_type` varchar(255) NOT NULL,
-`bet_won` boolean DEFAULT NULL,
-`payout_status` boolean DEFAULT NULL,
-`game_id` int(11) NOT NULL,
-`parlay_id` int(11) DEFAULT NULL,
-`user_id` int NOT NULL,
-PRIMARY KEY (`slip_id`),
-FOREIGN KEY games2bet_slips(`game_id`) 
-REFERENCES Games(`game_id`) 
-    ON UPDATE CASCADE 
-    ON DELETE CASCADE,
-    CHECK(Wager>=0),
-FOREIGN KEY users2bet_slips(`user_id`) 
-REFERENCES Users(`user_id`) 
-    ON UPDATE CASCADE 
-    ON DELETE CASCADE,
-CONSTRAINT CHK_bet_type CHECK(UPPER(bet_type) = 'TEAM_A_MONEY_LINE' OR 
-	UPPER(bet_type) = 'TEAM_B_MONEY_LINE' OR
-	UPPER(bet_type) = 'TEAM_A_SPREAD' OR
-	UPPER(bet_type) = 'TEAM_B_SPREAD' OR
-	UPPER(bet_type) = 'OVER' OR
-	UPPER(bet_type) = 'UNDER')
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
+('03.0','Chiefs','Buccaneers','149','-169','56.5');
 
 --
 -- Table structure for table `Users_bet_slips`
 --
 
 CREATE TABLE `Users_bet_slips` (
+`primary_key` int(11) NOT NULL AUTO_INCREMENT,
 `slip_id` int(11) NOT NULL,
-`user_id` int(11) NOT NULL,
-PRIMARY KEY (`slip_id`, `user_id`),
-FOREIGN KEY fk_slip(`slip_id`) REFERENCES Bet_slips(`slip_id`) ON UPDATE CASCADE ON DELETE CASCADE,
-FOREIGN KEY fk_user(`user_id`) REFERENCES Users(`user_id`) ON UPDATE CASCADE ON DELETE CASCADE
+`user_id` int(11),
+PRIMARY KEY (`primary_key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
-
-
---
--- Table structure for table `Parlay`
---
-CREATE TABLE `Parlay` (
-`parlay_id` int(11) NOT NULL AUTO_INCREMENT,
-`parlay_1` int(11) NOT NULL,
-`parlay_2` int(11) NOT NULL,
-PRIMARY KEY (`parlay_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE `Parlay_bet_slips` (
-`slip_id` int(11) NOT NULL,
-`parlay_id` int(11) NOT NULL,
-PRIMARY KEY (`slip_id`, `parlay_id`),
-FOREIGN KEY fk_p_slip(`slip_id`) REFERENCES Bet_slips(`slip_id`) ON UPDATE CASCADE ON DELETE CASCADE,
-FOREIGN KEY fk_parlay(`parlay_id`) REFERENCES Parlay(`parlay_id`) ON UPDATE CASCADE ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `Bet_slips`
---
-
-INSERT INTO `Bet_slips` (`wager`,`bet_type`,`game_id`, `parlay_id`, `user_id`) VALUES
-('10.00', 'TEAM_A_SPREAD',1, 1, 1),
-('10.00', 'TEAM_B_SPREAD',2, 1, 1),
-('10.00', 'OVER',1, NULL, 2);
 --
 -- Dumping data for table `Users_bet_slips`
 --
 INSERT INTO `Users_bet_slips` (`slip_id`, `user_id`) VALUES
-(33,22);
---
--- Dumping data for table `Parlay`
---
-INSERT INTO `Parlay` (`parlay_1`, `parlay_2`) VALUES
-(1,2),
-(2,3);
-
-INSERT INTO `Parlay_bet_slips` (`slip_id`, `parlay_id`) VALUES
-(111,222);
+(1,1);
